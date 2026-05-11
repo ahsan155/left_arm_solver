088502a78de51612e19acc0fa7ddffdf09907d28
Ahsan <ahmedahsan155@gmail.com>
2026-05-10 19:23:19 -0500

    added launch and config references
    -moveit_demo and main node launch added to data_files -referenced g1 robot xacro and config yaml files

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = "left_arm_solver"
@@ -9,11 +10,40 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", [
+            "launch/moveit_demo.launch.py",
+            "launch/waypoint_node.launch.py",
+        ]),
+        ("share/" + package_name + "/launch/moveit_launch", [
+            "launch/moveit_launch/move_group.launch.py",
+            "launch/moveit_launch/moveit_rviz.launch.py",
+            "launch/moveit_launch/rsp.launch.py",
+            "launch/moveit_launch/setup_assistant.launch.py",
+            "launch/moveit_launch/spawn_controllers.launch.py",
+            "launch/moveit_launch/static_virtual_joint_tfs.launch.py",
+            "launch/moveit_launch/warehouse_db.launch.py",
+        ]),
+        ("share/" + package_name + "/urdf", glob("urdf/*.urdf") + glob("urdf/*.xml") + glob("urdf/README.md")),
+        ("share/" + package_name + "/urdf/meshes", glob("urdf/meshes/*.STL")),
+        ("share/" + package_name + "/config", [
+            "config/waypoint_node.yaml",
+            "config/g1.ros2_control.xacro",
+            "config/g1.srdf",
+            "config/g1.urdf.xacro",
+            "config/initial_positions.yaml",
+            "config/joint_limits.yaml",
+            "config/kinematics.yaml",
+            "config/moveit_controllers.yaml",
+            "config/moveit.rviz",
+            "config/pilz_cartesian_limits.yaml",
+            "config/ros2_controllers.yaml",
+            "config/sensors_3d.yaml",
+        ]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="user",
-    maintainer_email="user@example.com",
+    maintainer="Ahsan Ahmed",
+    maintainer_email="ahsan@scaledrive.ai",
     description="MoveIt2 waypoint node for G1 left arm + Unitree arm_joint_cmd output.",
     license="BSD-3-Clause",
     tests_require=["pytest"],
